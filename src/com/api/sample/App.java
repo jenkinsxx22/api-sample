00f24165aecfe732dc45939f1fcbddf90107187b
engrnisarahmed <cengr.nisar.ahmed@gmail.com>
2022-11-15 21:30:11 +0500

    changes in app

--- src/com/api/sample/App.java
+++ src/com/api/sample/App.java
@@ -1,33 +1,32 @@
 package com.api.sample;
 
+import java.util.List;
+
 public class App {
 	
-	public void SaveEmployee(Employee emp) {
-		
+	public static void SaveEmployee(Employee emp) {
+		System.out.println("Student No:"+emp.getId());
+		System.out.println("Student Name:"+emp.getName());
+		System.out.println("Student Address:"+emp.getAddress());
 	}
 
-	public static void main(String args[]) {
+/*	public static void GetAllEmployees(List<Employee> elist) {
+		System.out.println("List of Employees");
+		for (int a=0; a<3; a++) {
+			System.out.println("Student Name: "+elist.indexOf(elist));
+		}
+	}*/
+	public static void main(String args[])  {
 		
 		Employee emp1 = new Employee(1,"Nisar","Sukkur");
 		Employee emp2 = new Employee(2,"Ahmed","Sukkur");
 		Employee emp3 = new Employee(2,"Abbasi","Sukkur");
+		SaveEmployee(emp1);
+		SaveEmployee(emp2);
+		SaveEmployee(emp3);
 
-	System.out.println(
-			"ID:"+emp1.getId()+
-			"Name:"+emp1.getName()+
-			"Address:"+emp1.getAddress()
-			);
-	System.out.println(
-			"ID:"+emp2.getId()+
-			"Name:"+emp2.getName()+
-			"Address:"+emp2.getAddress()
-			);
-	System.out.println(
-			"ID:"+emp3.getId()+
-			"Name:"+emp3.getName()+
-			"Address:"+emp3.getAddress()
-			);
 		
-		
+//		GetAllEmployees(emplist1);
+
 	}
 }
